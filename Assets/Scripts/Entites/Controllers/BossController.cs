a52508d75a47b30db5fc5b8986b906f81b5c7738
jelly1702 <urin1702@gmail.com>
2024-05-21 23:07:39 +0900

    Update BossController.cs
    불필요한 주석 및 디버그로그 삭제

--- Assets/Scripts/Entites/Controllers/BossController.cs
+++ Assets/Scripts/Entites/Controllers/BossController.cs
@@ -6,7 +6,6 @@ public class BossController : MonoBehaviour
 {
     private Animator animator;
 
-
     public void Start()
     {
         animator = GetComponent<Animator>();
@@ -16,7 +15,6 @@ public class BossController : MonoBehaviour
 
     private IEnumerator BossAttackRoutine()
     {
-        Debug.Log("애니메이션실행");
         while (true)
         {
             yield return new WaitForSeconds(4f);
@@ -24,34 +22,28 @@ public class BossController : MonoBehaviour
             int randomValue = Random.Range(0, 4);
             if (randomValue == 0)
             {
-                Debug.Log("어택1");
                 animator.SetTrigger("Attack1");
             }
             else if (randomValue == 1)
             {
-                Debug.Log("어택2");
                 animator.SetTrigger("Attack2");
             }
             else if (randomValue == 2)
             {
-                Debug.Log("어택3");
                 animator.SetTrigger("Attack3");
             }
         }
     }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        // 충돌한 오브젝트가 공인지 확인
         if (collision.gameObject.CompareTag("Ball"))
         {
-            Debug.Log("히트");
             animator.SetTrigger("BossHit");
         }
-        // 플레이어와 충돌했는지 확인
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("플레이어킬");
-            Destroy(collision.gameObject); // 플레이어 오브젝트를 파괴
+            Destroy(collision.gameObject);
         }
     }
 }
