50be16869ec6b9062da0374b362f4a615e4db2c3
jelly1702 <urin1702@gmail.com>
2024-05-22 14:34:37 +0900

    Update Controller.cs
    Ball, Boss, stage Controller.cs 수정

--- Assets/Scripts/Entites/Controllers/BossController.cs
+++ Assets/Scripts/Entites/Controllers/BossController.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class BossController : MonoBehaviour
 {
     private Animator animator;
+    private int ChangeBossIndex;
+    private BossHandler bossHandler;
+    private Coroutine CurrentPattern;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        bossHandler = GetComponent<BossHandler>();
+    }
 
     public void Start()
     {
-        animator = GetComponent<Animator>();
-        StartCoroutine(BossAttackRoutine());
-
+        
+        ChangeBossIndex = animator.GetLayerIndex("ChangeBoss Layer");
+        CurrentPattern = StartCoroutine(BossAttackPhase1());
+        bossHandler.BossPhase2 += BossChangePattern;
     }
 
-    private IEnumerator BossAttackRoutine()
+    private IEnumerator BossAttackPhase1()
     {
         while (true)
         {
-            yield return new WaitForSeconds(4f);
+            yield return new WaitForSeconds(5f);
 
             int randomValue = Random.Range(0, 4);
             if (randomValue == 0)
@@ -35,15 +44,38 @@ public class BossController : MonoBehaviour
         }
     }
 
-    public void OnCollisionEnter2D(Collision2D collision)
+    private IEnumerator BossAttackPhase2()
     {
-        if (collision.gameObject.CompareTag("Ball"))
+        animator.SetLayerWeight(ChangeBossIndex, 1);
+        animator.SetTrigger("ChangeBoss");
+        Time.timeScale = 0f;
+        yield return new WaitForSecondsRealtime(5f);
+        Time.timeScale = 1f;
+
+        while (true)
         {
-            animator.SetTrigger("BossHit");
+
+            yield return new WaitForSeconds(4f);
+
+            int randomValue = Random.Range(0, 4);
+            if (randomValue == 0)
+            {
+                //animator.SetTrigger("nell");
+            }
+            else if (randomValue == 1)
+            {
+                //animator.SetTrigger("");
+            }
+            else if (randomValue == 2)
+            {
+                //animator.SetTrigger("");
+            }
         }
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Destroy(collision.gameObject);
-        }
+    }
+
+    private void BossChangePattern()
+    {
+        StopCoroutine(CurrentPattern);
+        CurrentPattern = StartCoroutine(BossAttackPhase2());
     }
 }
